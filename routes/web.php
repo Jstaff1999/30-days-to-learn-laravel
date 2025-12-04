fd5272f0e44ce9e47aacea14e4f761ae0dedab1d
Jstaff1999 <Joshua.Stafford@rhinogroup.co.uk>
2025-12-04 16:27:53 +0000

    controller, model and migrations
    connected the project to its assigned database and made the migrations, models and seeders for the project

--- routes/web.php
+++ routes/web.php
@@ -2,33 +2,11 @@
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
 });
-
-class Job {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50.000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10.000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40.000'
-            ]
-        ];
-    }
-};
 
 Route::get('/jobs', function () {
     return view('jobs', [
@@ -37,7 +15,7 @@ Route::get('/jobs', function () {
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
+    $job = Job::find($id);
 
     return view('job', ['job' => $job]);
 });
